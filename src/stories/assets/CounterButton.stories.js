d2cc1d699caa4972612368932714a0cfbea5c54d
Giancarix117 <giancarlobetti2008@hotmail.com>
2023-02-17 01:25:33 +0100

    all done

--- src/stories/assets/CounterButton.stories.js
+++ src/stories/assets/CounterButton.stories.js
@@ -2,15 +2,15 @@
 
 import React from "react";
 
-import About from "../../components/About";
+import CounterButton from "../../components/CounterButton";
 
 export default {
   /* 👇 The title prop is optional.
    * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
    * to learn how to generate automatic titles
    */
-  title: "About",
-  component: About,
+  title: "CounterButton",
+  component: CounterButton,
 };
 
-export const Basic = () => <About />;
+export const Basic = () => <CounterButton />;
